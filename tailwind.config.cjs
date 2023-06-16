47441d3bf3c04b479a1a2b022283ba323b6b8270
Yash Thakur <thakur.yash514@gmail.com>
2023-06-16 23:43:41 +0530

    first initial changes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,13 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: ["class", '[data-theme="dark"]'],
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				dark: "#111111",
+				light: "#FFFFFF",
+				blue: "#33d2ff",
+				gray: "#C4C4C4",
+				"dark-gray": "#1A1A1A",
+			},
+			fontFamily: {
+				inter: ["Inter", "sans-serif"],
+				outfit: ["Outfit", "sans-serif"],
+			},
+		},
 	},
-	plugins: [require("@tailwindcss/typography"),require("daisyui")],
+	plugins: [require("@tailwindcss/typography"), require("daisyui")],
 	daisyui: {
 		themes: true, // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
 		darkTheme: "dark", // name of one of the included themes for dark mode
 		logs: false, // Shows info about daisyUI version and used config in the console when building your CSS
-	  }
-}
+	},
+};
